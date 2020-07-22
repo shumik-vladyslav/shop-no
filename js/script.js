e3ff8b2b8aa7447ce1be20dcf8046be3ad2d5a7d
shumik-vladyslav <shumik.vladyslav@gmail.com>
2020-07-22 11:45:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// $(function () {
+//     $('#elastislide').elastislide();
+// });
+//
+// $(document).ready(function () {
+//     $('#button_map').on('click', function () {
+//        $('#map').toggle();
+//     });
+// });
+
+// function myFunction() {
+//     var x = document.getElementById('map');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// }
+
+// $('#button_map').click(function () {
+//     $('#map').toggle('explode');
+// });
+
+
+// $( document ).ready(function(){
+//     $('#button_map').click(function(){
+//         $('#map').toggle(1000);
+//     });
+// });
+
